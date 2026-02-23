a3c423f7f0e94af46a29de062f7dc26ebff7fcd2
Elias Jääsaari <elias.jaasaari@gmail.com>
2026-02-23 22:53:17 +0200

    Update example.py

--- example.py
+++ example.py
@@ -3,19 +3,22 @@ import numpy as np
 from lemur import Lemur
 from lemur.maxsim import MaxSim
 
-# train: np.ndarray float32, shape (num_corpus_token_embeddings, dim)
-# train_counts: np.ndarray uint64, shape (num_corpus_documents, )
-# test: np.ndarray float32, shape (num_query_token_embeddings, dim)
-# test_counts: np.ndarray uint64, shape (num_query_documents, )
+# train: torch.tensor float32, shape (num_corpus_token_embeddings, dim)
+# train_counts: torch.tensor uint64, shape (num_corpus_documents, )
+# test: torch.tensor float32, shape (num_query_token_embeddings, dim)
+# test_counts: torch.tensor uint64, shape (num_query_documents, )
 # train_counts is an array containing the number of token embeddings for each corpus document
 
-lemur = Lemur(index="lemur_index", device="cpu")
+lemur = Lemur(index="lemur_index", device="cpu")  # or "cuda" or "mps"
 lemur.fit(
     train=train,
     train_counts=train_counts,
     epochs=10,
     verbose=True,
 )
+
+# Set epochs = 0 to skip training the MLP
+# This still works well but usually requires 2-4x more candidates to rerank
 
 # 1) Compute features
 feats = lemur.compute_features((test, test_counts))
@@ -41,3 +44,8 @@ reranked = ms.rerank_subset(
     k_final,
     cand_np,
 )
+
+print(reranked)
+
+# Compute weights for new points
+new_W = lemur.compute_weights(new_docs, new_docs_counts)
